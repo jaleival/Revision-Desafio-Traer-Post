753e0b1b5abf83ce395dca1419611e3f3dad5d2b
Jorge Leiva <jorge.leiva.l@gmail.com>
2024-04-04 02:30:37 -0300

    :tada: Subida Desafío Traer Posts. Revisión Clases

---
+++ README.md
@@ -0,0 +1,1 @@
+# Revision-Desafio-Traer-Post
---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const obtenerPosts = async () => {
+    const api = 'https://jsonplaceholder.typicode.com/posts';
+    const postData = document.getElementById("post-data");
+    try {
+        const response = await fetch(api);
+        const data = await response.json();
+        console.log(data);
+        data.forEach((e) => {
+            // Envío los primeros 10 posts
+            if (e.id <= 10) {
+                postData.innerHTML += ` 
+                                        <div>
+                                            <ul>
+                                                <li>
+                                                    <strong>${e.title}</strong><br>
+                                                    <span>${e.body}</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        `
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+document.getElementById("btn").addEventListener("click", () => {
+    obtenerPosts();
+});
